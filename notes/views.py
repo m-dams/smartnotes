6cb197f3e84019e00faabb763ac381744b17f139
DAMS <dams.michal.jakub@gmail.com>
2022-10-17 10:48:41 +0200

    Building dynamic webpages

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.views.generic import DetailView, ListView
+from .models import Notes
 
-# Create your views here.
+
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+
+
+def detail(request, pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("Note does not exist")
+    return render(request, 'notes/notes_detail.html', {'note': note})
